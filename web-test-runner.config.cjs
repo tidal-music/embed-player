095bb30dacc3538da3d1ee7a03b3e11f2a17468c
Jeremy Karlsson <jeremyk@tidal.com>
2024-05-29 13:06:52 +0200

    Add executablePath

--- web-test-runner.config.cjs
+++ web-test-runner.config.cjs
@@ -1,5 +1,5 @@
 const testRunner = require('@web/test-runner-playwright');
 
 module.exports = {
-  browsers: [testRunner.playwrightLauncher({ product: 'chromium' })],
+  browsers: [testRunner.playwrightLauncher({ product: 'chromium', launchOptions: { executablePath: '/home/runner/.cache/ms-playwright/chromium-1117/chrome-linux/chrome' } })],
 };
