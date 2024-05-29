f6b32ce074ace3518b1dcd8c66ba38234864ed46
Jeremy Karlsson <jeremyk@tidal.com>
2024-05-29 12:54:44 +0200

    Lock playwright to 1.22.2

--- web-test-runner.config.cjs
+++ web-test-runner.config.cjs
@@ -1,5 +1,5 @@
 const testRunner = require('@web/test-runner-playwright');
 
 module.exports = {
-  browsers: [testRunner.playwrightLauncher({ product: 'chromium', launchOptions: { executablePath: '/home/runner/.cache/ms-playwright/chromium-1117' } })],
+  browsers: [testRunner.playwrightLauncher({ product: 'chromium' })],
 };
